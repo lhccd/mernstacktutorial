94dfb1c513b57d7d42bc71eeca63e8dd2056257e
Lorenz Dang <lorenz.dang@tum.de>
2020-06-16 16:23:11 +0200

    uploaded whole backlog to finish mern stack tutorial

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,4 +17,21 @@ router.route('/add').post((req,res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').delete((req,res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req,res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            user.username = req.body.username;
+            user.save()
+                .then(() => res.json('User updated!'))
+                .catch(err => res.status(400).json('Error: '+ err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
