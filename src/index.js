c9d122f60a26bf71d734d4aae12f46b057b8a35e
Lorenz Dang <lorenz.dang@tum.de>
2021-11-15 17:23:48 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+class Hello extends React.Component{
+    render(){
+        return (
+            <h1 className={'title'}> Hello {this.props.name} </h1>
+        )
+    }
+}
+
+//ReactDOM.render(<Hello name={"World"}/>, document.getElementById('root'))
 ReactDOM.render(<App />, document.getElementById('root'));
